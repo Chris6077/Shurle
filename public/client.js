47be20d3901a82f67a271ae4065469baff3d6af2
Christian Winkler <realgoldencoldlp@gmail.com>
2018-03-27 04:45:14 +0200

    added html

--- public/client.js
+++ public/client.js
@@ -3,6 +3,7 @@
 
 // by default, you've got jQuery,
 // add other scripts at the bottom of index.html
+var api = false;
 $(document).ready(function(){
   $('.btn-shorten').on('click', function(){
     $.ajax({
@@ -23,5 +24,17 @@ $(document).ready(function(){
           }
       }
     });
+  });
+  $('.api').on('click', function(){
+    if(api){
+      $('.container')[0].innerHTML = '<div class="input-group input-group-lg"> <input id="url-field" type="text" class="form-control" placeholder="Paste a link..."> <span class="input-group-btn"> <button class="btn btn-shorten" type="button">SHORTEN</button> </span> </div> <div id="link"></div>';
+      $('.api')[0].innerHTML = "API Usage";
+      api = false;
+    }
+    else {
+      $('.container')[0].innerHTML = "<h2>Example inputs:</h2> <p>https://shurle.glitch.me/new/google.com</p> <p>https://shurle.glitch.me/new/http://www.google.com</p> <p>https://shurle.glitch.me/new/http://google.com</p> <p>https://shurle.glitch.me/new/www.google.com</p> <h2>These inputs will return this:</h2> <p>{ \"shortUrl\": \"shurle.glitch.me/1\" }</p> <h2> This service differs HTTPS and HTTP and automatically adds HTTP if no protocol is given. Works with mailto etc. too.</h2>";
+      $('.api')[0].innerHTML = "Back to Shurle";
+      api = true;
+    }
   });
 });
